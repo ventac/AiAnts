80ac6284ad8d73f654b98bddef18644a5f872ad2
Guilherme Ventapane <62478174+ventac@users.noreply.github.com>
2022-12-14 10:22:34 +0000

    Ajouté fonction MoveAnt et LeaveTrace

--- AiAnts.c
+++ AiAnts.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 // Définition des dimensions du tableau
-
-#define X_TABLEAU 8
-#define Y_TABLEAU 8
+#define COL_QTT 8
+#define ROW_QTT 8
 
 // Déclaration en globale pour l'instant
-int tableau[X_TABLEAU][Y_TABLEAU];
+int table[ROW_QTT][COL_QTT];
 
 // Remplir le tableau avec 1 partout
 // TODO : Ajouter d'autres chiffres pour répresenter der murs/chemins plus complexes
-void RemplirTableau(/*int * tableau[][]*/){
-    for (int ligne = 0; ligne < X_TABLEAU; ligne++){
-        for (int col = 0; col < Y_TABLEAU; col++){
-            tableau[ligne][col] = ligne;
+void FillTable(/*int * tableau[][]*/){
+    for (int ligne = 0; ligne < ROW_QTT; ligne++){
+        for (int col = 0; col < COL_QTT; col++){
+            table[ligne][col] = 0;
         }
     }
     //return tableau;
 }
 
 // Afficher le tableau
-void AfficherTableau(){
-    for (int ligne = 0; ligne < X_TABLEAU; ligne++){
-        for (int col = 0; col < Y_TABLEAU; col++){
-            printf("%d \t", tableau[ligne][col]);
+void ShowTable(){
+    for (int ligne = 0; ligne < ROW_QTT; ligne++){
+        for (int col = 0; col < COL_QTT; col++){
+            printf("%d \t", table[ligne][col]);
         }
         printf("\n");
     }
@@ -32,14 +31,11 @@ void AfficherTableau(){
 int main(void){
 
 
-    RemplirTableau();
-    AfficherTableau();
+    FillTable();
+    ShowTable();
     
     // Pour tester, la position initielle sera le coin inférieur gauche (8,0)
     // et l'objectif sera le coin supérieur droite (0,8)
-
-    
-
 
 
 }
