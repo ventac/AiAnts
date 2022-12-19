51da1c0db450aee827b65d9f36899dd5143fab72
DUEE-MOREAU Arwen <arwen.duee.moreau@gmail.com>
2022-12-19 14:55:57 +0100

    changements

--- Buffer.c
+++ Buffer.c
@@ -2,16 +2,16 @@
 
 
 // Afficher le tableau
-void ShowTable(){
+void ShowTable(int cells[],int row,int col){
     //system("cls");  // Windows
     system("clear");  //nix (Mac aussi?)
-    for (int ligne = 0; ligne < ROW_QTT; ligne++){
-        for (int col = 0; col < COL_QTT; col++){
-            if (table[ligne][col] == F){
+    for (int ligne = 0; ligne < row; ligne++){
+        for (int colone = 0; colone < col; colone++){
+            if (cells[ligne][colone] == F){
                 printf("🐜\tTEST");
             }
             else
-                printf("%d\t", table[ligne][col]);
+                printf("%d\t", cells[ligne][colone]);
         }
         printf("\n");
     }
