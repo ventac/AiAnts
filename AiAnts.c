a71e9f0592a02233dddd227aad4839eec03e3168
DUEE-MOREAU Arwen <arwen.duee.moreau@gmail.com>
2022-12-20 13:29:44 +0100

    adding structure

--- AiAnts.c
+++ AiAnts.c
@@ -4,6 +4,7 @@
 //#include <windows.h> // Pour Windows
 #include <unistd.h>  // Pour Linux (Et mac?)
 #include  <string.h>
+#include <stdbool.h>
 
 // print the table with ant and path
 
@@ -20,6 +21,24 @@
 #define ROW_QTT 8
 #define ANT 500
 */
+
+// struct for ants
+struct Ant{
+    int direction; // where is the head pointing? +x, -x, +y, -y
+    int pher_toFood; // number of pheromones left when going to pick food
+    int pher_toHome; // number of pheromones left when going back home
+    bool HasFood;
+    int pos_x; // current x position on the map
+    int pos_y; // current y position on the map
+};
+
+// struct for each tile of the map
+struct Tile{
+    int num_toFood; // number of pheromones on the tile when going to pick food
+    int num_toHome; // number of pheromones on the tile when going back home
+    bool obstacle; // True if there's an obstacle on the tile
+};
+
 
 // Code principal
 int main(void){
